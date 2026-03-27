0c1f202b8f946d38cf2d91bf0f79684f193114bc
WooGeun-Nam <rolru7686@gmail.com>
2026-03-27 19:53:20 +0900

    push

--- 백준/W_11727.cpp
+++ 백준/W_11727.cpp
@@ -1,54 +1,33 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
-int main() {
-    
-    // n : 선수 수 , k : k년 만큼 진행
-    int n, k;
-    cin >> n >> k;
-    
-    // 포지션은 11까지 존재 하므로 11+1
-    vector<priority_queue<int>> player(12);
-    
-    // 선수 수 만큼 입력 받기
-    // 선수별 포지션, 선수 가치
-    for(int i=0; i<n; i++)
+int main()
+{
+    int n;
+    cin >> n;
+
+    // n 크기 만큼 (테케 1회)
+    vector<int> dp(n + 1, 0);
+
+    // 초기값 설정
+    dp[1] = 1;
+    if (n >= 2)
     {
-        int position;
-        int value;
-        cin >> position >> value;
-        
-        player[position].push(value);
+        // 세로2개
+        // 가로2개
+        // 2x2 정사각형1개
+        dp[2] = 3;
     }
-    
-    // K년 까지 선수 가치 하락 계산
-    for(int i=0; i<k; i++)
+
+    // 상향식
+    for (int i = 3; i <= n; i++)
     {
-        for(int j=1; j<=11; j++)
-        {
-            // 해당 포지션에 선수가 1명이라도 있으면 계산
-            if (!player[j].empty()) {
-                int value = player[j].top();
-                player[j].pop();
-                if (value > 0) value--;
-                player[j].push(value);
-            }
-        }
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
     }
-    
-    int answer = 0;
-    // 마지막 선수 가치가 가장 높은 선수 선발
-    for(int i=1; i<=11; i++)
-    {
-        if (!player[i].empty()) {
-            answer += player[i].top();
-        }
-    }
-    
-    cout << answer;
-    
+
+    cout << dp[n] << endl;
+
     return 0;
 }
